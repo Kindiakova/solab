efbd8c6ed81c7a0b6f2b5684d2509864610f39b6
Diana Kindiakova <75929765+Kindiakova@users.noreply.github.com>
2024-04-29 00:21:11 +0300

    Update Hash.java

--- src/Hash.java
+++ src/Hash.java
@@ -6,13 +6,49 @@ import java.util.PriorityQueue;
 import java.math.BigInteger;
 import parcs.*;
 
-public class Hash implements AM
+public class CountPalindromes implements AM
 {
     private static long startTime = 0;
 
     public static int countPalindromes(String s)
     {
-        int res = 1;
+        int pos_f = 0;
+        int pos_s = 0;
+        for (int j=0; j<s.length(); j+=1)
+            {
+                if (s.charAt(j) == ' ' && pos_f == 0)
+                {
+                    pos_f = j;
+                } else 
+                if (s.charAt(j) == ' ')
+                {
+                    pos_s = j;
+                }
+            }
+        int start = Integer.valueOf(s.substring(0, pos_f));
+        int step = Integer.valueOf(s.substring(pos_f+1, pos_s));
+        s = s.substring(pos_s+1, s.length());
+        System.err.println("Calculating (in function) for .. "+start+" "+step+" "+s);
+        int n = s.length();
+        int res = 0;
+        int l = start;
+        while (l<n)
+        {
+            int j = 0;
+            while (l - j >= 0 && l + j < n && s.charAt(l - j) == s.charAt(l + j))
+            {
+                res += 1;
+                j += 1;
+            }
+            j = 0;
+            while (l - j >= 0 && l + j + 1 < n && s.charAt(l - j) == s.charAt(l + j + 1))
+            {
+                res += 1;
+                j += 1;
+            }
+            l += step;
+        }
+        System.err.println("Result (in function) is " + res);
         return res;
     }
 
@@ -55,9 +91,7 @@ public class Hash implements AM
         int[] results = new int[numberWorkers];
         for (int i = 0; i < numberWorkers; i++)
         {
-            results[i] = channels[i].readInt();
-            System.out.println("Result[i]: " + results[i]);
-            
+            results[i] = (int) channels[i].readObject();
         }
         System.err.println("Calculation of the result");
         int res = 0;
@@ -75,6 +109,8 @@ public class Hash implements AM
 
     public void run(AMInfo info)
     {
+        //int start_pos = (int)info.parent.readObject();
+        //int step = (int)info.parent.readObject();
         String s = (String)info.parent.readObject();
         System.err.println("Started run with " + s + " string");
         int subresult = countPalindromes(s);
